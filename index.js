c137f90739f7bcd8b3bb67237fe7769b13339f11
Giamo Lao <giamolao98@gmail.com>
2020-12-20 13:09:43 -0500

    ability to write file

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data)
+{
+  const readmeMD = generateMarkdown(data);
+  fs.writeFile(('./'+fileName), readmeMD, err =>
+    {
+      if (err) throw new Error(err);
+      console.log("README created! Check out ./"+fileName+" to see it!");
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init()
+{
+  // Get input from user
+  inquirer
+    .prompt(questions)
+    .then(answers => postPrompter(answers))
+    .catch(error => errorHandler(error));
+}
+
+// Handle the answers to the initial inquirer prompt
+function postPrompter(answers)
+{
+  // TODO: Check if the user wants to change anything and change it
+
+  // Save file
+  let fileName = "README.md";
+  inquirer
+    .prompt
+    (
+      [
+        {
+          type: 'input',
+          name: 'fileName',
+          message: 'Save as ("<filename>.md"):',
+          validate: a => {return a ? true : 'Please enter "<filename>.md"!'}
+        }
+      ]
+    )
+    .then(data => {fileName = data.fileName;})
+    .catch(error => errorHandler(error));
+
+  writeFile(fileName, answers);
+}
+
+// Handle any errors that occur from the inquirer prompt
+function errorHandler(error)
+{
+  if (error.isTtyError)
+    console.log("Error: Prompt couldn't be rendered in the current environment.");
+  else
+    console.log("Error:", error);
+}
 
 // Function call to initialize app
 init();
